dd3c55836406b5a959fa51f8ee09e4d453a12b39
Mark Burns <markthedeveloper@gmail.com>
2015-12-08 07:10:02 +0900

    fix decrypt padding

--- lib/pass/decrypt.ex
+++ lib/pass/decrypt.ex
@@ -1,5 +1,4 @@
 defmodule Pass.Decrypt do
-
   def run(encrypted_contents, password) do
     case Pass.KeyAndIvGenerator.run(encrypted_contents, password) do
       {_, key, iv, encrypted} -> decrypt(key, iv, encrypted)
@@ -13,14 +12,22 @@ defmodule Pass.Decrypt do
   def decrypt(key, iv, encrypted) do
     result = unpad(:crypto.aes_cbc_256_decrypt(key, iv, encrypted))
 
-    {:ok, result}
+    check result
   rescue
     message ->
       encrypted = encrypted |> Base.encode16
       {:error, "Couldn't decrypt bytes #{inspect message} #{encrypted}"}
   end
 
-  def unpad(binary) do
+  defp check(result) do
+    if String.printable? result do
+      {:ok, result}
+    else
+      {:error, "invalid decryption"}
+    end
+  end
+
+  defp unpad(binary) do
     case  String.printable?(binary)  do
       true -> binary
 
