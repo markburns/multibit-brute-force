51a996b026b35fe23ef165bedf09aa0918a537ef
Mark Burns <markthedeveloper@gmail.com>
2015-12-09 08:51:54 +0900

    build executable

--- mix.exs
+++ mix.exs
@@ -15,7 +15,9 @@ defmodule Pass.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :tzdata]]
+    [applications: [:logger,
+        #   :tzdata
+      ]]
   end
 
   # Dependencies can be Hex packages:
