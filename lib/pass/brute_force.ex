77ea1da99579e12817cde6f3b5be52cff3e0f4f8
Mark Burns <markthedeveloper@gmail.com>
2015-12-08 12:04:27 +0900

    add support for displaying progress

--- lib/pass/brute_force.ex
+++ lib/pass/brute_force.ex
@@ -2,9 +2,12 @@ defmodule Pass.BruteForce do
   def run(encrypted_filename, passwords_file) do
     encrypted_contents = File.read! encrypted_filename
 
+    total_passwords = num_passwords_from(passwords_file)
+    IO.puts "Total passwords: #{total_passwords}"
+
     try do
       File.stream!(passwords_file, [])
-      |> Pass.Parallel.map &(try_password(encrypted_contents, &1))
+      |> Pass.Parallel.map &(try_password(encrypted_contents, &1)), total_passwords
 
       {:error, "No valid password found"}
     catch
@@ -12,7 +15,28 @@ defmodule Pass.BruteForce do
     end
   end
 
-  def try_password(encrypted_contents, {password, index}) do
+  defp num_passwords_from(f) do
+    IO.puts "calculating total number of passwords..."
+
+    System.cmd("wc", [f]) |> _extract_number_from_wc
+  end
+
+  defp _extract_number_from_wc({item, _}) do
+    String.split(item, ~r/\s+/)
+    |> line_count_from
+    |> String.to_integer
+  end
+
+  def display(i) do
+    IO.inspect i
+    i
+  end
+
+  def line_count_from(columns) do
+    Enum.at columns, 1
+  end
+
+  def try_password(encrypted_contents, {password, _}) do
     password = String.rstrip(password)
     result = Pass.Decrypt.run encrypted_contents, password
 
--- lib/pass/parallel.ex
+++ lib/pass/parallel.ex
@@ -3,9 +3,15 @@ defmodule Pass.Parallel do
   Parallelizes the calculation of `fun` mapped over `collection`
 
   ## Examples
-    iex> Pass.Parallel.map([1,2], fn({e, _})-> e + 2 end)
+    iex> Pass.Parallel.map([1,2], fn({e, _})-> e + 2 end, 2)
     [3,4]
   """
+
+  def map(collection, fun, total) do
+    collection
+    |> parallelize(self, fun, total)
+    |> collate_results
+  end
 
   def map(collection, fun) do
     collection
@@ -13,10 +19,32 @@ defmodule Pass.Parallel do
     |> collate_results
   end
 
+
+  defp parallelize(list, me, fun, total) do
+    list
+    |> Stream.with_index
+    |> display(total)
+    |> Stream.map &(calculate_individual(me, fun, &1))
+  end
+
   defp parallelize(list, me, fun) do
     list
     |> Stream.with_index
     |> Stream.map &(calculate_individual(me, fun, &1))
+  end
+
+
+  defp display(stream, total) do
+    {el, index} = Stream.take(stream, 1) |> Enum.to_list |> List.first
+
+    if rem(index, 1000) == 0 do
+      IO.puts inspect el
+      IO.puts "#{index} / #{total}"
+      percent = 100.0 * index / total
+      IO.puts "#{inspect percent}%"
+    end
+
+    stream
   end
 
   defp calculate_individual(me, fun, function_args) do
